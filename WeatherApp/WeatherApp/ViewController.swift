7d45c321ef20486f87c490390e62375ab54a67a4
Erick Silvério Blass <erick1junior@hotmail.com>
2024-09-08 23:34:32 -0300

    Config inicial do projeto

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Erick Silvério on 08/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
