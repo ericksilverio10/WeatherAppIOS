cb9325996a6a4d52d2555f393110690f769f7a4a
Erick Silvério Blass <erick1junior@hotmail.com>
2024-09-10 20:27:57 -0300

    Card de vento e umidade

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -53,6 +53,72 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var humidityLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Umidade"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = UIColor(named: "secondaryColor")
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
+    private lazy var humidityValueLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "1000mm"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = UIColor(named: "secondaryColor")
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
+    private lazy var humidityStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [humidityLabel, humidityValueLabel])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var windLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Vento"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = UIColor(named: "secondaryColor")
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
+    private lazy var windValueLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "10km/h"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = UIColor(named: "secondaryColor")
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
+    private lazy var windStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [windLabel, windValueLabel])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [humidityStackView, windStackView])
+        stackView.axis = .vertical
+        stackView.backgroundColor = UIColor(named: "softGray")
+        stackView.spacing = 3
+        stackView.layer.cornerRadius = 10
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 12, leading: 24, bottom: 12, trailing: 24)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     // view controller carregou
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,8 +140,6 @@ class ViewController: UIViewController {
     }
     
     private func setupView() {
-        view.backgroundColor = UIColor.red
-        
         setHierarchy()
         setConstraints()
     }
@@ -83,6 +147,8 @@ class ViewController: UIViewController {
     private func setHierarchy() {
         view.addSubview(backgroundView)
         view.addSubview(headerView)
+        view.addSubview(containerStackView)
+        
         headerView.addSubview(cityLabel)
         headerView.addSubview(temperatureLabel)
         headerView.addSubview(temperatureIcon)
@@ -120,6 +186,34 @@ class ViewController: UIViewController {
             temperatureIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor, constant: 15),
             temperatureIcon.heightAnchor.constraint(equalToConstant: 85),
             temperatureIcon.widthAnchor.constraint(equalToConstant: 85),
+        ])
+        
+        NSLayoutConstraint.activate([
+            temperatureLabel.topAnchor.constraint(equalTo: cityLabel.safeAreaLayoutGuide.bottomAnchor, constant: 15),
+            temperatureLabel.leadingAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.leadingAnchor, constant: 25),
+            temperatureLabel.trailingAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.trailingAnchor, constant: -25),
+            
+            temperatureIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -25),
+            temperatureIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor),
+            temperatureIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor, constant: 15),
+            temperatureIcon.heightAnchor.constraint(equalToConstant: 85),
+            temperatureIcon.widthAnchor.constraint(equalToConstant: 85),
+        ])
+        
+        NSLayoutConstraint.activate([
+            containerStackView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 24),
+            containerStackView.widthAnchor.constraint(equalToConstant: 206),
+            containerStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            humidityStackView.widthAnchor.constraint(equalToConstant: 206),
+            humidityStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            windStackView.widthAnchor.constraint(equalToConstant: 206),
+            windStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            
+            
+            
         ])
     }
 }
